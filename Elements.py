8cade4a6fed359da63f1ff9c68a76feb11a284d4
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-01-12 21:14:57 +0545

    added elements files

--- Elements.py
+++ Elements.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -18,6 +19,13 @@ driver.get('http://trytestingthis.netlify.app/')
 driver.find_element(By.ID, 'fname').send_keys("Shreyash")
 driver.find_element(By.ID, 'lname').send_keys("Poudel")
 
+# select gender
+driver.find_element(By.ID, 'male').click()
+
+# select option
+select_element = driver.find_element(By.NAME, 'Optionwithcheck[]')
+select = Select(select_element)
+select.select_by_visible_text('Option 2')
 
 
 time.sleep(2)
