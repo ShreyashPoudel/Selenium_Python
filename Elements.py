97abeb8273facbbc9337a172a6cb0609447e350f
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-01-12 20:22:45 +0545

    Initial commit

---
+++ Elements.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# driver.get('https://www.google.com')
+# googleSearchBox = driver.find_element(By.ID, 'APjFqb')
+# googleSearchBox.send_keys('Automation')
+# # driver.find_element(By.NAME,'btnK').click()
+# googleSearchBox.send_keys(Keys.RETURN)
+
+driver.get('http://trytestingthis.netlify.app/')
+# fill firstname and lastname
+driver.find_element(By.ID, 'fname').send_keys("Shreyash")
+driver.find_element(By.ID, 'lname').send_keys("Poudel")
+
+
+
+time.sleep(2)
